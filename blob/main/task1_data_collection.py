e60ecdece8b401772cfa330fca6230b2864c880c
shireeshkumarit <110602575+shireeshkumarit@users.noreply.github.com>
2026-04-12 20:29:03 +0530

    Update task1_data_collection.py

--- blob/main/task1_data_collection.py
+++ blob/main/task1_data_collection.py
@@ -20,51 +20,52 @@ try:
 
   response = requests.get(url, headers=headers)
   top_stories = response.json()
-  print(type(top_stories))
+  # print(type(top_stories))
   top_stories_500 = top_stories[:500]
-  print(top_stories_500)
+  # print(top_stories_500)
 except Exception as e:
   print("error occured while getting the Top stories IDs",e)
   exit()
 # top 500 stories
+try:
+  stories_by_category = []
+  for id in top_stories_500:
+    response = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{id}.json', headers=headers)
+    story = response.json()
+    # print(story)
+    for category, keywords in categories.items():
+      # print(f"Category: {category}, Keyword : {keywords}")
+      for keyword in keywords:
+        # print(keyword.lower())
+        # print(story.get("title").lower())
+        if keyword.lower() in story.get("title").lower():
+          # print(keyword)
+          stories_by_category.append({
+              "post_id": story.get("id"),
+              "title": story.get("title"),
+              "category": category,
+              "score": story.get("score"),
+              "num_comments": story.get("descendants"),
+              "author": story.get("by"),
+              "collected_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+          })
+    # time.sleep(0.5)
+except Exception as e:
+  print(f"error occured while getting the Story details for story id : {id}",e)
+  exit()
 
-
-stories_by_category = []
-for id in top_stories_500:
-  response = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{id}.json', headers=headers)
-  story = response.json()
-  # print(story)
-  for category, keywords in categories.items():
-    # print(f"Category: {category}, Keyword : {keywords}")
-    for keyword in keywords:
-      # print(keyword.lower())
-      # print(story.get("title").lower())
-      if keyword.lower() in story.get("title").lower():
-        # print(keyword)
-        stories_by_category.append({
-            "post_id": story.get("id"),
-            "title": story.get("title"),
-            "category": category,
-            "score": story.get("score"),
-            "num_comments": story.get("descendants"),
-            "author": story.get("by"),
-            "collected_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-        })
-  # time.sleep(0.5)
-  
-  # data = json.loads(stories_by_category)
-  # print(json.dumps(data,indent=2))
-  # print(stories_by_category)
-  
-# print(json.dumps(stories_by_category, indent=2))
-
-# Create a data folder
+# Create a data folder and a filename with the current day
 os.makedirs("data", exist_ok=True)
-
 filename = f"data/trends_{datetime.now().strftime('%Y%m%d')}.json"
 
+# convert list to a dataframe and restrict to 25 rows per category
+df = pd.DataFrame(stories_by_category)
+df_limited = df.groupby('category').head(25)
+
+# convert dataframe to list and add those to a json file
+df_limited_list = df_limited.to_dict(orient='records')
+
 with open(filename, "w") as f:
-  json.dump(stories_by_category, f, indent=4)
+  json.dump(df_limited_list, f, indent=4)
 
-print(f"Collected {len(stories_by_category)} stories. Saved to {filename}")
-
+print(f"Collected {len(df_limited_list)} stories. Saved to {filename}")
